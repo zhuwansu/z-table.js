addedc6c2e7087ab6d4d25d40fc70d1467fe84fe
zhuwansu <zhuwansu@chicv.com>
2019-12-05 16:42:20 +0800

    add js

---
+++ TB.Task.js
@@ -0,0 +1,152 @@
+﻿(function (n, t, i) {
+    var u, r;
+    parseFloat(jQuery.fn.jquery) < 2 && (u = n.MutationObserver || n.WebKitMutationObserver || n.MozMutationObserver ||
+        function () {
+            return {
+                observe: function () { }
+            }
+        }, new u(function (n) {
+            for (var r, t, i = 0, u = n.length; i < u; i++) r = n[i], r.addedNodes.length && (t = r.addedNodes[0]).tagName == "SCRIPT" && t.src && t.src.indexOf("jQuery") > -1 && (t.onerror = function () {
+                return this.onload(), !1
+            })
+        }).observe(document.getElementsByTagName("head")[0], {
+            childList: !0,
+            subtree: !1,
+            attributes: !1,
+            characterData: !1
+        }));
+    r = {
+        args: [],
+        onRun: function () { },
+        onSuccess: null,
+        onError: null,
+        onComplete: null,
+        onProgress: null,
+        interval: 0,
+        filter: function (n, t) {
+            return n && t == "success"
+        },
+        dialog: {
+            options: {},
+            onClose: null,
+            formatter: function (n) {
+                var t = n.data;
+                return t || (t = {
+                    Message: "\u672a\u77e5\u9519\u8bef\uff01"
+                }), t.Message || t.message || t
+            },
+            message: !0
+        }
+    };
+    i.task = function () {
+        var a = null,
+            e = 0,
+            n = {},
+            o = 0,
+            f = {
+                success: 0,
+                error: 0,
+                leave: e
+            },
+            h = null,
+            u = t.Deferred(),
+            y = [],
+            c;
+        if (arguments[0] instanceof Array && typeof arguments[1] == "function") t.extend(!0, n, r, {
+            args: arguments[0],
+            onRun: arguments[1]
+        });
+        else if (typeof arguments[0] == "object") t.extend(!0, n, r, arguments[0]);
+        else throw new TypeError("\u53c2\u6570\u9519\u8bef\uff01");
+        if (e = f.leave = n.args.length, c = function (t) {
+            a = n.args[o];
+            h = n.onRun.call(u, a, o);
+            h.index = o;
+            h.item = a;
+            h.always(function (i, r, s) {
+                f.leave--;
+                if (i.Success === false) {
+                    r = "error";
+                }
+                r === "success" && n.filter.call(u, i, r, s) === !0 ? f.success++ : (s = i, i = null, r = "error", f.error++);
+                y.push(i);
+                u.notify({
+                    status: r,
+                    total: e,
+                    index: s.index,
+                    data: i,
+                    arg: s.item,
+                    progress: f.success + f.error,
+                    success: f.success,
+                    error: f.error
+                });
+                f.leave <= 0 ? u.resolve({
+                    success: f.success,
+                    error: f.error,
+                    total: e,
+                    datas: y
+                }) : t || (o++ , setTimeout(c, n.interval))
+            })
+        }, u.run = n.interval && n.interval > 0 ?
+            function () {
+                e && c()
+            } : function () {
+                for (; o < e; o++) c(!0)
+            }, u.options = function () {
+                return n
+            }, n.dialog) {
+            var s = t("<div><\/div>").css({
+                position: "relative",
+                height: "100%"
+            }),
+                l = t("<div><\/div>").css({
+                    position: "relative",
+                    padding: "10px 5px",
+                    "font-weight": "bold"
+                }).appendTo(s),
+                v = t("<div class='scrollbar'><\/div>").css({
+                    overflow: "auto",
+                    "border-top": "solid 1px #ddd",
+                    "line-height": "24px",
+                    position: "absolute",
+                    top: "38px",
+                    left: "0",
+                    bottom: "0",
+                    right: "0",
+                    padding: "0 5px",
+                    display: n.dialog.message ? "block" : "none"
+                }).appendTo(s);
+            l.html("\u6b63\u5728\u51c6\u5907\u6267\u884c\u4efb\u52a1...");
+            u.progress(function (t) {
+                l.html("\u8bf7\u7a0d\u5019\uff0c\u6b63\u5728\u6267\u884c\u4efb\u52a1\uff1a" + t.progress + "/" + t.total + " ...");
+                n.dialog.message && (v.append("<div>" + n.dialog.formatter.call(u, t) + "<\/div>"), v[0].scrollTop = v[0].scrollHeight)
+            }).done(function (t) {
+                l.html("\u6267\u884c\u5b8c\u6210\u3002<span style='color:#008000'>\u6210\u529f\uff1a" + t.success + "<\/span>\uff0c<span style='color:#f00'>\u5931\u8d25\uff1a" + t.error + "<\/span>\uff01");
+                t.error > 0 && l.append('<div class="panel-tool"><a href="javascript:;" class="panel-tool-close"><\/a><\/div>').find(".panel-tool-close").click(function () {
+                    n.dialog.onClose && n.dialog.onClose.call(u);
+                    n.dialog.dom.dialog("close")
+                })
+            });
+            u.dialog = function () {
+                s.dialog.apply(s, arguments)
+            };
+            t.fn.dialog ? i.dialog(t.extend(!0, {
+                width: 350,
+                height: n.dialog.message ? 200 : 38
+            }, n.dialog, {
+                    closable: !1,
+                    content: s,
+                    border: !1,
+                    noheader: !0,
+                    style: {
+                        "border-radius": 2
+                    }
+                })) : s.appendTo(document.body)
+        }
+        return n.onProgress && u.progress(n.onProgress), u.done(function (t) {
+            t.error == 0 && n.onSuccess && n.onSuccess.call(u, t);
+            t.error > 0 && n.onError && n.onError.call(u, t);
+            n.onComplete && n.onComplete.call(u, t)
+        }), u
+    }
+})(window, jQuery, erp);
---
+++ z-table.js
@@ -0,0 +1,362 @@
+﻿(function ($) {
+    'use strict';
+
+    function tableUtil($table) {
+        this.version = 1.0;
+        this.prefix = "z";
+        if (!($table instanceof jQuery)) $table = $($table);
+        this.$table = $table;
+        this.templates = {
+            tr: `<tr class="${this.prefix}"></tr>`,
+            td: `<td class="${this.prefix}td"></td>`,
+            opTd: `<td class="${this.prefix}op-td"></td>`,
+            input: `<input class="${this.prefix}input">`,
+            checkbox: `<input type="checkbox" class="${this.prefix}check"><span class="${this.prefix}check"></span>`,
+            file: `<input class="${this.prefix}input" type="file">`,
+            uploadImg: `<div class="${this.prefix}uploadImg"></div><input class="${this.prefix}uploadImg" type="file" multiple>`,
+            textarea: `<textarea class="${this.prefix}textarea"></textarea>`,
+            th: `<th></th>`,
+            select: `<select class="${this.prefix}select"></select>`,
+            span: `<span class="${this.prefix}span"></span>`,
+            thead: `<thead><tr class="text-center"></tr></thead>`,
+            a: `<a class="${this.prefix}a" href="javascript:;" ></a>`,
+            add: `<a class=" ${this.prefix}add" href="javascript:;" >新增</a>`,
+            del: `<a class=" ${this.prefix}del" href="javascript:;" >删除</a>`,
+            br: `</br>`,
+            option: `<option></option>`,
+            $div: $(`<div></div>`),
+            span2: `<${this.prefix}span></${this.prefix}span>`,
+        };
+        this.labels = {
+            input: this.templates.input,
+            text: this.templates.input,
+            checkbox: this.templates.checkbox,
+            file: this.templates.file,
+            uploadImg: this.templates.uploadImg,
+            textarea: this.templates.textarea,
+            select: this.templates.select,
+            spanInput: this.templates.input,
+        };
+        this.option = { debug: true }
+        this.options = { appendBtn: true };
+        this.rowHtml = this.$table.find('tbody').html();
+        this.rowIndex = 0;
+        this.events = new event();
+        function event() {
+            this.onAddRow = null;
+            this.onFillData = null;
+            this.onAppendRow = null;
+            this.afterAppendBtn = null;
+        }
+        this.$table.find('tbody').html('');
+        //注册增加和删除事件
+        var that = this;
+        this.$table
+            .on('click', `.${this.prefix}add`, function () {
+                that.addRow();
+            })
+            .on('click', `.${this.prefix}del`, function () {
+                that.removeRow($(this));
+            });
+        this.countTr = function () {
+            return this.$table.find('tbody>tr[rowIndex]').length;
+        };
+        this.generateFormItem = function (col, header) {
+            var inputType = col.inputType;
+            if (!col.inputType) inputType = 'spanInput';
+
+            var name = col.name;
+            var attr = col.inputAttr;
+            var placeHolder = col.placeHolder;
+            var data = col.data;
+            if (header) {
+                inputType = col.headerInput.inputType;
+                name = col.headerInput.name;
+                attr = col.headerInput.attr;
+                placeHolder = col.headerInput.placeHolder;
+                data = col.headerInput.data;
+            }
+
+            var res = $(this.labels[inputType]);
+            if (attr && attr.length > 0) {
+                $.each(attr, function (index, item) {
+                    res.attr(item.name, item.value);
+                });
+            }
+            res.attr('placeholder', placeHolder);
+            res.attr('name', name);
+
+            if (inputType == 'select') {
+                var $option = $(this.templates.option);
+                $option.val('');
+                $option.html(placeHolder);
+                res.append($option);
+                if (data && data.length > 0) {
+                    $.each(data, function (sIndex, sItem) {
+                        var $option = $(that.templates.option).html(sItem.name);
+                        $option.attr("value", sItem.id);
+                        res.append($option);
+                    });
+                }
+            }
+            res.addClass(`${this.prefix}${inputType}`);
+            return res;
+        };
+        this.refreshRowIndex = function () {
+            while (true) {
+                var last = this.$table.find('tr[rowIndex="' + this.rowIndex + '"]');
+                if (last.length > 0) return last;
+
+                this.rowIndex--;
+                if (this.rowIndex < 0) {
+                    break;
+                }
+            }
+        }
+        this.initRowTemplate = function (containerSelector) {
+            if (containerSelector) this.rowHtml = $(containerSelector + " tbody").html();
+
+            if (this.options && this.options.cols && this.options.cols.length > 0) {
+                var $tr = $(this.templates.tr);
+                var cols = this.options.cols;
+                $.each(cols, function (index, col) {
+                    var $td = $(that.templates.td);
+                    $td.attr(`${that.prefix}columnId`, col.id);
+                    $td.attr(`${that.prefix}typeId`, col.typeId);
+                    $td.html(that.generateFormItem(col));
+                    $tr.append($td);
+                });
+                this.rowHtml = $tr[0].outerHTML;
+            }
+        }
+        this.initColumns = function (cols) {
+            if (cols) this.options.cols = cols;
+            var $thead = $(this.templates.thead);
+            $.each(cols, function (index, col) {
+                var $th = $(that.templates.th);
+                var $span = $(that.templates.span);
+                if (!col.displayName) {
+                    col.displayName = col.name;
+                }
+                $span.html(col.displayName);
+                $th.append($span);
+
+                //head input
+                if (col.headerInput && col.input) {
+                    if (col.headerInput === true) {
+                        col.headerInput = {
+                            inputType: col.inputType,
+                            placeHolder: col.placeHolder,
+                            name: col.name,
+                            data: col.data
+                        };
+                    } else {
+                        if (!col.headerInput.name) {
+                            col.headerInput.name = col.name;
+                        }
+                        if (!col.headerInput.inputType) {
+                            col.headerInput.inputType = col.inputType;
+                        }
+                        if (!col.headerInput.placeHolder) {
+                            col.headerInput.placeHolder = col.placeHolder;
+                        }
+                        if (!col.headerInput.data) {
+                            col.headerInput.data = col.data;
+                        }
+                    }
+                    var $input = that.generateFormItem(col, true);
+
+                    $th.append(that.templates.br);
+                    $th.append($input);
+                }
+
+                $thead.find('tr').append($th);
+            });
+
+            this.initRowTemplate();
+            $thead.find('tr').append('<th><span>操作</span></th>>');
+            this.$table.html($thead);
+            return this;
+        }
+        this.appendBtn = function ($tr) {
+            if (!$tr) $tr = this.refreshRowIndex();
+            if (!$tr || !$tr.length || $tr.length == 0) return;
+
+            var $td = $tr.find(`.${this.prefix}op-td`);
+            if ($td.length == 0) {
+                $td = $(this.templates.opTd);
+                $tr.append($td);
+            }
+            $td.html('');
+            var $add = $(this.templates.add);
+            $td.append($add);
+            $td.append(this.templates.br);
+            var $del = $(this.templates.del);
+            $td.append($del);
+
+            if (this.events.afterAppendBtn) {
+                this.events.afterAppendBtn(this, $add, $del);
+            }
+        }
+        // add row and render it to html by use rowHtml
+        this.addRow = function (data) {
+            //check rowHtml
+            if (!this.rowHtml) {
+                //try init row by cols
+                this.initRowTemplate();
+            }
+
+            var $row = this.doAddRow(this.rowHtml);
+            if (data) {
+
+                //行填充数据
+                var $controls = $row.find(`input,select,textarea,${this.prefix}span`);
+                for (var i = 0; i < $controls.length; i++) {
+                    var $control = $($controls[i]);
+                    var nodeName = $control.prop('nodeName').toLowerCase();
+                    var name = $control.attr(this.prefix + 'name');
+                    if (!name) {
+                        name = $control.attr('name');
+                    }
+                    var value = $control.val();
+                    var type = $control.attr('type');
+                    var oldV;
+                    var v = oldV = data[name];
+
+                    if (nodeName == 'input' && type == 'radio') {
+                        //radio
+                        if (value == v) {
+                            $control.attr('checked', 'checked');
+                        }
+                    } else if (nodeName == `${this.prefix}span`) {
+                        //span
+                        $control.html(v);
+                    } else if (nodeName == 'input' || nodeName == 'select' || nodeName == 'textarea') {
+                        // form control
+                        $control.val(v);
+                    }
+                }
+                $row.data('oldValue', data);
+            }
+
+            if (this.events.onAddRow) {
+                this.events.onAddRow(this.rowIndex, $row);
+            }
+
+            return $row;
+        }
+        // add rows <see f: addRow>
+        this.addRows = function (data) {
+            if (data && data.length > 0) {
+                $.each(data, function (index, item) {
+                    that.addRow(item);
+                });
+
+                for (var i = 0; i < this.plugins.length; i++) {
+                    var plugin = this.plugins[i];
+                    if (plugin.onPostBody) {
+                        plugin.onPostBody();
+                    }
+                }
+            }
+        }
+        // add row by row html
+        this.doAddRow = function () {
+            this.rowIndex++;
+            var $newRow = $(this.rowHtml);
+            $newRow.attr('rowIndex', this.rowIndex);
+            $newRow.addClass(this.prefix + 'tr');
+
+            //name 增加 rowIndex
+            $newRow.find(`input,select,textarea,${this.prefix}span`).attr(`${this.prefix}name`, function () {
+                return $(this).attr('name');
+            });
+            $newRow.find(`input,select,textarea,${this.prefix}span`).attr('name', function (n, v) {
+                return v + that.rowIndex;
+            });
+            var $tbody = this.$table.find('tbody');
+            if ($tbody.length == 0) {
+                $tbody = $('<tbody></tbody>');
+                this.$table.append($tbody);
+            }
+            $tbody.append($newRow);
+
+            //添加新行后增加新增和删除按钮
+            this.appendBtn($newRow, true, true);
+            return $newRow;
+        }
+        this.getRowIndex = function ($this) {
+            var rowIndex = null;
+            var step = $this;
+            while (true) {
+                if (!step) break;
+                rowIndex = step.attr('rowIndex');
+                if (rowIndex && rowIndex >= 0) {
+                    return rowIndex;
+                }
+                step = step.parent();
+            }
+        }
+        //remove current row
+        this.removeRow = function ($this) {
+            var rowIndex = this.getRowIndex($this);
+            this.removeRowByIndex(rowIndex);
+        }
+        //removeRow by rowIndex
+        this.removeRowByIndex = function (rowIndex) {
+            this.$table.find(`tr[rowIndex="${rowIndex}"]`).remove();
+        }
+        // generate a form serialize object for every row , like this [{},{}]
+        this.rows = function (onRowCallback, onFieldCallBack) {
+            var params = [];
+            $('tr[rowIndex]').each(function (index, item) {
+                var oldValue = $(item).data('oldValue');
+                var rowIndex = $(item).attr('rowIndex');
+                var obj = {};
+                if (oldValue) obj.id = oldValue.id;
+
+                $.each($(item).find(`input,select,textarea,${that.prefix}span`), function (cIndex, cItem) {
+                    var name = $(cItem).attr(that.prefix + 'name');
+
+                    var val = $(cItem).val();
+                    if (cItem.nodeName == `${that.prefix}span`.toUpperCase()) {
+                        val = $(cItem).text();
+                    }
+                    if (cItem.type == 'radio') {
+                        if (name && cItem.checked === true) {
+                            obj[name] = val;
+                        }
+                    } else if (name) {
+                        obj[name] = val;
+                    }
+
+                    if (onFieldCallBack) {
+                        onFieldCallBack(obj, cItem);
+                    }
+                });
+
+
+                $.extend(obj, { rowIndex: rowIndex });
+                if (onRowCallback) {
+                    onRowCallback(obj, $(item));
+                }
+                params.push(obj);
+            });
+            if (this.debug()) {
+                console.log("table-ext.js:rows");
+                console.log(JSON.stringify(params));
+            }
+            return params;
+        }
+        this.debug = function () {
+            return this.options.debug;
+        }
+    }
+
+    $.fn.extend({
+        zTable: function () {
+            return new tableUtil($(this));
+        }
+    });
+
+}(window.jQuery));
